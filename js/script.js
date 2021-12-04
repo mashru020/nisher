a6cf9a71084707460fa097bcafff9b65fe35baf2
mashru020 <mashrurrahman16@gmail.com>
2021-12-04 21:11:38 +0600

    exp and edu page added

--- js/script.js
+++ js/script.js
@@ -1,11 +1,12 @@
 'use strict'
 const a = document.querySelector('.wrapper');
-const menu = document.querySelector('.about');
+const menu = document.querySelector('.about--btn');
 console.log(menu);
-const section1 = document.querySelector('#about');
+const section1 = document.querySelector('#section--2');
 
 menu.addEventListener('click', function() {
     section1.scrollIntoView({behavior: 'smooth'});
     a.classList.add('static');
 });
+
 
